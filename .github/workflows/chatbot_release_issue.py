d65a2592a516fb1eb5e775bb7da678ae8aeb251d
Shingo Kobayashi <shingo.kobayashi@sansan.com>
2023-04-07 17:55:47 +0900

    argments長をprint

--- .github/workflows/chatbot_release_issue.py
+++ .github/workflows/chatbot_release_issue.py
@@ -6,8 +6,9 @@ print('Start')
 
 # Argument check
 arguments = sys.argv
-if len(arguments) != 2:
-    sys.exit('arguments is not found')
+print(f'arguments len is {len(arguments)}')
+# if len(arguments) != 2:
+#     sys.exit('arguments is not found')
 comment = arguments[1]
 
 print(f'comment = {comment}')
