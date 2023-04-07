5661481946d913d1a089198428a54367b09caf15
Shingo Kobayashi <shingo.kobayashi@sansan.com>
2023-04-07 18:38:34 +0900

    コメントをpostするように

--- .github/workflows/chatbot_release_issue.py
+++ .github/workflows/chatbot_release_issue.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import os
 import sys
+import subprocess
 import re
+
+def post_comment(issue_number, content_path, version):
+    with open(content_path) as f2:
+        issue_template = f2.read()
+        subprocess.call(f'gh issue comment {issue_number} --body {issue_template}', shell=True)
 
 print('Start')
 
 # Argument check
 arguments = sys.argv
-if len(arguments) != 2:
+if len(arguments) < 3:
     sys.exit('arguments is not found')
 
-command, release_type, version = arguments[1].split(' ')
+issue_number = arguments[1]
+command, release_type, version = arguments[2].split(' ')
 print(f'command = {command}, release_type = {release_type}, version = {version}')
 
 if command != '/chatbot':
@@ -24,8 +30,9 @@ if len(re.findall(r"([0-9]+)\.([0-9]+)\.([0-9]+)", version)) != 1:
     sys.exit('invalid version. please set like v1.2.3')
 
 # Post comment
-with open(".github/ISSUE_TEMPLATE/planned_release.md") as f2:
-    issue_template = f2.read()
-    print(issue_template)
+if release_type == 'planned':
+    post_comment(issue_number, '.github/ISSUE_TEMPLATE/planned_release.md', version)
+elif release_type == 'hotfix':
+    post_comment(issue_number, '.github/ISSUE_TEMPLATE/hotfix_release.md', version)
 
 print('Finish')
