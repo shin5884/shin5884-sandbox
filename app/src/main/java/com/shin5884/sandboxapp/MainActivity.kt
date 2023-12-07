3ece78369b25c53354b59eabd623c927b51642bc
Shingo Kobayashi <shingo.kobayashi@sansan.com>
2023-12-07 18:47:38 +0900

    適当コミット

--- app/src/main/java/com/shin5884/sandboxapp/MainActivity.kt
+++ app/src/main/java/com/shin5884/sandboxapp/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.shin5884.sandboxapp.ui.theme.SandboxappTheme
-
+// ほげほげ
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
