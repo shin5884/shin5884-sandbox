09822cf0f88260006bcb28e42013a085cc6749f3
Shingo Kobayashi <shingo.kobayashi@sansan.com>
2023-12-08 14:34:06 +0900

    pythonの変数展開は$いらなかった

--- .github/workflows/create_pr.py
+++ .github/workflows/create_pr.py
@@ -7,9 +7,9 @@ import re
 # Create master merge PR
 print('\n------Create PR-------')
 release_ver = '9.10.0'
-pr_title = f'${release_ver}リリースブランチ'
-pr_body = f'レビュー対象はBump version to ${release_ver}のコミットからです。\n'
-command = f'gh pr create --title ${pr_title} --body ${pr_body} --label release --draft'
+pr_title = f'{release_ver}リリースブランチ'
+pr_body = f'レビュー対象はBump version to {release_ver}のコミットからです。\n'
+command = f'gh pr create --title {pr_title} --body {pr_body} --label release --draft'
 proc = subprocess.Popen(command,
                         shell=True,
                         stdin = subprocess.PIPE,
